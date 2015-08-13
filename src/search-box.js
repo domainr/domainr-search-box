a8f5908e465247b42f712f0dfc80ee307b77e075
Ian Gilman <ian@iangilman.com>
2015-08-13 14:26:15 -0700

    Limiting status calls to 10 domains

--- src/search-box.js
+++ src/search-box.js
@@ -122,8 +122,9 @@ SearchBox.prototype = {
 
     // Extract domains without status
     var d = [];
+    var statusLimit = 10;
     var rs = this._state.results;
-    for (var i = 0; i < rs.length; i++) {
+    for (var i = 0; i < rs.length && d.length < statusLimit; i++) {
       var r = rs[i];
       r.status = this._cache[r.domain + ':status'] || r.status;
       if (!r.status) {
